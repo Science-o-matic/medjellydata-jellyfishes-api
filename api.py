839d4a9922ae71e06af50e60c3f890f9364a396a
ygneo <antonio.barcia@gmail.com>
2014-05-10 12:41:50 +0200

    Make jellyfishes_by_beach do actual requests to medjelly API, returning jellyfishes hazard list

--- api.py
+++ api.py
@@ -1,20 +1,24 @@
-# MedJelly API interface module
 import os
 import settings
 import random
+import requests
+import base64
 
 
-class MedJellyAPI:
+def jellyfishes_by_beach(beach_id, lang):
+    url = "%(base_url)s/beaches/beach/%(id)i/lang/%(lang)s/device/%(device)s" % (
+        {'base_url': settings.MEDJELLY_API_BASE_URL,
+         'id': beach_id,
+         'lang': lang,
+         'device': settings.DEVICE_ID,
+         })
+    headers = {"Authorization": "Basic %s" % _auth_token()}
 
-    @staticmethod
-    def jellyfishes_by_beach(beach_id):
-        """ Fake method returning random jellyfishes list """
-        jellyfishes = []
-        for n in range(random.randrange(1, 3)):
-            jelly = {
-                'id': random.randrange(1, 13),
-                'risk_level': random.randrange(1, 3)
-                }
-            jellyfishes.append(jelly)
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()
+    return r.json()["jellyFishesHazard"]
 
-        return jellyfishes
+
+def _auth_token():
+    auth_string = "%s:%s" % (settings.MEDJELLY_API_USER, settings.MEDJELLY_API_PASSWORD)
+    return base64.b64encode(auth_string)
